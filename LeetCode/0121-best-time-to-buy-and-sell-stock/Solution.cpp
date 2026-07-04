64b13ec7c2dca94b65e0724337b144e68a421d89
Harshit-harry09 <209129291+Harshit-harry09@users.noreply.github.com>
2026-07-04 14:00:00 +0530

    feat(LeetCode): add solution for #0121 - Best Time To Buy And Sell Stock

---
+++ LeetCode/0121-best-time-to-buy-and-sell-stock/Solution.cpp
@@ -0,0 +1,19 @@
+// Problem: Best Time to Buy and Sell Stock
+// Problem ID: 0121
+// Difficulty: Easy
+// LeetCode URL: https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+// Language: cpp
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int mini = prices[0];
+        int profit = 0;
+        for(int i = 1;i<prices.size();i++){
+            int cost = prices[i]-mini;
+            profit = max(profit,cost);
+            mini = min(mini,prices[i]);
+        }
+        return profit;
+    }
+};
