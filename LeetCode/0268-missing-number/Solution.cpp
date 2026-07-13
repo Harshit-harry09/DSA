3510f6f9f425e0bb8562e9fedda98b9cbe432feb
Harshit-harry09 <209129291+Harshit-harry09@users.noreply.github.com>
2026-07-13 14:00:00 +0530

    feat(LeetCode): add solution for #0268 - Missing Number

---
+++ LeetCode/0268-missing-number/Solution.cpp
@@ -0,0 +1,18 @@
+// Problem: Missing Number
+// Problem ID: 0268
+// Difficulty: Easy
+// LeetCode URL: https://leetcode.com/problems/missing-number/
+// Language: cpp
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int xor1 =0,xor2 =0;
+        int n = nums.size();
+    for(int i =0;i<n;i++){
+        xor2 = xor2 ^ nums[i];
+        xor1 = xor1 ^ (i+1);
+    }
+    return xor1 ^ xor2;
+    }
+};
