1a69d33fe10be8f0c87d801f0980ac0249334649
Harshit-harry09 <209129291+Harshit-harry09@users.noreply.github.com>
2026-07-16 14:00:00 +0530

    feat(LeetCode): add solution for #0326 - Power Of Three

---
+++ LeetCode/0326-power-of-three/Solution.cpp
@@ -0,0 +1,15 @@
+// Problem: Power of Three
+// Problem ID: 0326
+// Difficulty: Easy
+// LeetCode URL: https://leetcode.com/problems/power-of-three/
+// Language: cpp
+
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+         if(n<=0)return false;
+        if(n==1)return true;
+        if(n%3 != 0) return false;
+        return isPowerOfThree(n/3);
+    }
+};
