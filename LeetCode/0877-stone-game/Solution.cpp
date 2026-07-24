f80e881c5bd00af16ccb4fc236465da4bc68c99c
Harshit-harry09 <209129291+Harshit-harry09@users.noreply.github.com>
2026-07-24 14:00:00 +0530

    feat(LeetCode): add solution for #0877 - Stone Game

---
+++ LeetCode/0877-stone-game/Solution.cpp
@@ -0,0 +1,12 @@
+// Problem: Stone Game
+// Problem ID: 0877
+// Difficulty: Medium
+// LeetCode URL: https://leetcode.com/problems/stone-game/
+// Language: cpp
+
+class Solution {
+public:
+    bool stoneGame(vector<int>& piles) {
+        return true;
+    }
+};
