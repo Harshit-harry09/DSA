2064b340c3a03c954b2905ae89ee4af5d22ba96b
Harshit-harry09 <209129291+Harshit-harry09@users.noreply.github.com>
2026-08-02 14:00:00 +0530

    feat(LeetCode): add solution for #2396 - Strictly Palindromic Number

---
+++ LeetCode/2396-strictly-palindromic-number/Solution.cpp
@@ -0,0 +1,12 @@
+// Problem: Strictly Palindromic Number
+// Problem ID: 2396
+// Difficulty: Medium
+// LeetCode URL: https://leetcode.com/problems/strictly-palindromic-number/
+// Language: cpp
+
+class Solution {
+public:
+    bool isStrictlyPalindromic(int n) {
+        return false;
+    }
+};
