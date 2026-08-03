c04e30dbd5482179942a65b173007d679103d21b
Harshit-harry09 <209129291+Harshit-harry09@users.noreply.github.com>
2026-08-03 14:00:00 +0530

    feat(LeetCode): add solution for #2769 - Find The Maximum Achievable Number

---
+++ LeetCode/2769-find-the-maximum-achievable-number/Solution.cpp
@@ -0,0 +1,12 @@
+// Problem: Find the Maximum Achievable Number
+// Problem ID: 2769
+// Difficulty: Easy
+// LeetCode URL: https://leetcode.com/problems/find-the-maximum-achievable-number/
+// Language: cpp
+
+class Solution {
+public:
+    int theMaximumAchievableX(int num, int t) {
+        return num + 2*t;
+    }
+};
