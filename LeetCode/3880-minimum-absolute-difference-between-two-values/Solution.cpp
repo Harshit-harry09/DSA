8c49f9d4d49b1a5de8c62912c241f955d3a09ead
Harshit-harry09 <209129291+Harshit-harry09@users.noreply.github.com>
2026-08-05 14:00:00 +0530

    feat(LeetCode): add solution for #3880 - Minimum Absolute Difference Between Two Values

---
+++ LeetCode/3880-minimum-absolute-difference-between-two-values/Solution.cpp
@@ -0,0 +1,30 @@
+// Problem: Minimum Absolute Difference Between Two Values
+// Problem ID: 3880
+// Difficulty: Easy
+// LeetCode URL: https://leetcode.com/problems/minimum-absolute-difference-between-two-values/
+// Language: cpp
+
+class Solution {
+public:
+    int minAbsoluteDifference(vector<int>& nums) {
+        int last1 = -1, last2 = -1;
+        int ans = INT_MAX;
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] == 1) {
+                last1 = i;
+                if (last2 != -1) {
+                    ans = min(ans, abs(last1 - last2));
+                }
+            }
+            else if (nums[i] == 2) {
+                last2 = i;
+                if (last1 != -1) {
+                    ans = min(ans, abs(last1 - last2));
+                }
+            }
+        }
+
+        return (ans == INT_MAX) ? -1 : ans;
+    }
+};
