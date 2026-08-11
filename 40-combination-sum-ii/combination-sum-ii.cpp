a9d706bee9a5fd93771445336bf5437c217ddd8a
Harshit-harry09 <fakeharry009@gmail.com>
2026-08-11 14:22:03 +0530

    Time: 0 ms (100.00%) | Memory: 13.9 MB (85.43%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.cpp
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
-void fun1(int ind,int target,vector<int>&ds,vector<int>&arr,vector<vector<int>>&ans){
-    if(target ==0){
- ans.push_back(ds);
-         return;
+void fun1(int ind,int target,vector<int>&arr,vector<int>&ds,vector<vector<int>>&ans){
+    if(target == 0){
+        ans.push_back(ds);
+        return;
     }
-
-    for(int i = ind;i<arr.size();i++ ){
-        if(i>ind && arr[i] == arr[i-1]) continue;
-            if(arr[i]>target) break;
+    for(int i = ind;i<arr.size();i++){
+        if(i>ind && arr[i] == arr[i-1])continue;
+        if(arr[i]>target)break;
         ds.push_back(arr[i]);
-        fun1(i+1,target-arr[i],ds,arr,ans);
+        fun1(i+1,target-arr[i],arr,ds,ans);
         ds.pop_back();
     }
 }
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
         sort(candidates.begin(),candidates.end());
-        vector<vector<int>>ans;
         vector<int>ds;
-        fun1(0,target,ds,candidates,ans);
+        vector<vector<int>>ans;
+        fun1(0,target,candidates,ds,ans);
         return ans;
     }
 };
